f359688fb975567d840a3cc714b47b58bc68c97f
Shashank Prakash <98454860+anonymous0905@users.noreply.github.com>
2025-05-01 18:24:35 +0530

    typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "PESU Simulation Suit",
+  title: "PESU Simulation Suite",
   description: "Faculty Dashboard",
 };
 
